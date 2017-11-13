d453f1ef3750480666eee11028430fa4b80e3570
Jorge Garcia <jorgegarcia525@gmail.com>
2017-11-13 04:50:24 -0600

    website syntax error fixes

--- save-data.js
+++ save-data.js
@@ -7,7 +7,7 @@ function saveData() {
 	var latitude = document.getElementById("latitude").value;
 	var longitude = document.getElementById("longitude").value;
 	// sessionStorage.setItem(latitude, longitude);
-	var blob = new Blob([latitude, longitude],{type = "text/plain;chairset=utf-8"});
+	var blob = new Blob([latitude, longitude],{type: "text/plain;chairset=utf-8"});
 	saveAs(blob, "input-file.txt")
 }
 
