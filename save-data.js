e2ec53b1c4894d96de7d70489963fa024fdbdb09
Jorge Garcia <jorgegarcia525@gmail.com>
2017-11-13 04:47:01 -0600

    added changes to website input

--- save-data.js
+++ save-data.js
@@ -1,12 +1,14 @@
 function doFirst() {
-	var button = documnet.getElementById("button");
+	var button = document.getElementById("button");
 	button.addEventListener("click", saveData, false);
 }
 
 function saveData() {
 	var latitude = document.getElementById("latitude").value;
 	var longitude = document.getElementById("longitude").value;
-	sessionStorage.setItem(latitude, longitude);
+	// sessionStorage.setItem(latitude, longitude);
+	var blob = new Blob([latitude, longitude],{type = "text/plain;chairset=utf-8"});
+	saveAs(blob, "input-file.txt")
 }
 
 window.addEventListener("load", doFirst, false)
